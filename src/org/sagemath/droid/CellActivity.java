c1c0d0c4339a216064830257a91fe698f377adb5
Ralf Stephan <ralf@ark.in-berlin.de>
2014-01-13 08:51:33 +0100

    Add action_new icons, add sparse menu with action_new and option menu, use it in CellActivity. Part of fix to #6.

--- src/org/sagemath/droid/CellActivity.java
+++ src/org/sagemath/droid/CellActivity.java
@@ -4,10 +4,19 @@ import org.sagemath.droid.CellGroupsFragment.OnGroupSelectedListener;
 
 import sheetrock.panda.changelog.ChangeLog;
 import sheetrock.panda.changelog.SimpleEula;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.android.actionbarcompat.ActionBarActivity;
 
@@ -15,6 +24,7 @@ public class CellActivity
 		extends ActionBarActivity 
 		implements OnGroupSelectedListener{
 	private final static String TAG = "CellActivity";
+	private static final String DIALOG_NEW_CELL = "newCell";
 
 	private ChangeLog changeLog;
 	
@@ -60,14 +70,50 @@ public class CellActivity
 		}
 	}
 	
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	switch (item.getItemId()) {
-    	case android.R.id.home:
-    		finish();
-    		return true;
-    	}
-        return super.onOptionsItemSelected(item);
-    }
-	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater menuInflater = getMenuInflater();
+		menuInflater.inflate(R.menu.sparse, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Uri uri;
+		Intent intent;
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			finish();
+			return true;
+		case R.id.menu_add: {
+			FragmentManager fm = this.getSupportFragmentManager();
+			NewCellDialog dialog = new NewCellDialog();
+			dialog.show(fm, DIALOG_NEW_CELL);
+			return true;
+		}
+		case R.id.menu_search:
+			Toast.makeText(this, "Tapped search", Toast.LENGTH_SHORT).show();
+			return true;
+		case R.id.menu_changelog:
+			changeLog.getFullLogDialog().show();
+			return true;
+		case R.id.menu_about_sage:
+			uri = Uri.parse("http://www.sagemath.org");
+			intent = new Intent(Intent.ACTION_VIEW, uri); 
+			startActivity(intent); 
+			return true;
+		case R.id.menu_manual_user:
+			uri = Uri.parse("http://www.sagemath.org/doc/tutorial/");
+			intent = new Intent(Intent.ACTION_VIEW, uri); 
+			startActivity(intent); 
+			return true;
+		case R.id.menu_manual_dev:
+			uri = Uri.parse("http://www.sagemath.org/doc/reference/");
+			intent = new Intent(Intent.ACTION_VIEW, uri); 
+			startActivity(intent); 
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 }
