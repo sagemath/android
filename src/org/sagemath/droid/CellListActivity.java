8440e2c9822dbf7c43fef06fc23e58e0db9ccb34
Ralf Stephan <ralf@ark.in-berlin.de>
2014-02-02 09:23:29 +0100

    on phones after cell discard, go back to base

--- src/org/sagemath/droid/CellListActivity.java
+++ src/org/sagemath/droid/CellListActivity.java
@@ -51,6 +51,13 @@ public class CellListActivity
 
 	
 	@Override
+	public void onResume() {
+		super.onResume();
+		if (CellCollection.getInstance().getCurrentGroup().isEmpty())
+			this.onBackPressed();
+	}
+		
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater menuInflater = getMenuInflater();
 		menuInflater.inflate(R.menu.sparse, menu);
