24f407db2079e795ef1789f9e5ce1b28811666b0
Nikhil Peter Raj <nikhilpeter93@gmail.com>
2014-07-14 17:46:19 +0530

    Add constants and strings

--- src/org/sagemath/droid/constants/StringConstants.java
+++ src/org/sagemath/droid/constants/StringConstants.java
@@ -15,23 +15,23 @@ public class StringConstants {
     public static final String PATH_PERMALINK = "permalink";
 
     //--SCHEMES--
-    public static final String SCHEME_WS = "ws";
     public static final String SCHEME_HTTPS = "https";
     public static final String SCHEME_HTTP = "http";
 
-    public static final String PROGRESS_INTENT = "progress-intent";
     public static final String ARG_PROGRESS_START = "progress-start";
     public static final String ARG_PROGRESS_END = "progress-end";
 
-    public static final String ID="id";
-    public static final String FLAG_INPUT_EMPTY="inputEmptyFlag";
+    public static final String FILE_IMAGE_OUTPUT = "imageOutput.png";
 
-    public static final String MATHJAX_CONFIG ="<script type=\"text/x-mathjax-config\">\n" +
+    public static final String ID = "id";
+    public static final String FLAG_INPUT_EMPTY = "inputEmptyFlag";
+
+    public static final String MATHJAX_CONFIG = "<script type=\"text/x-mathjax-config\">\n" +
             "  MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\\\(','\\\\)']]}});\n" +
             "</script>";
-    public static final String MATHJAX_CDN ="<script type=\"text/javascript\"\n" +
+    public static final String MATHJAX_CDN = "<script type=\"text/javascript\"\n" +
             "  src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML\">\n" +
             "</script>";
-    public static final String IMAGE_STYLE="<style>img{display: inline; height: auto; max-width: 100%;}</style>";
+    public static final String IMAGE_STYLE = "<style>img{display: inline; height: auto; max-width: 100%;}</style>";
 
 }
